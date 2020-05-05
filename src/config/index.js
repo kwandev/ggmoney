adfdf86789cb5e68ccc294d067caae35ffbfa3b7
khwan <rudghks7816@gmail.com>
2020-05-05 16:03:14 +0900

    feat: config 추가

--- src/config/index.js
+++ src/config/index.js
@@ -8,6 +8,17 @@ class Config {
   get GG_API_KEY() {
     return process.env.VUE_APP_GG_API_KEY
   }
+
+  get isMobile() {
+    const filter = 'win16|win32|win64|mac'
+
+    if (navigator.platform) {
+      if (filter.indexOf(navigator.platform.toLowerCase()) === -1) {
+        return true
+      }
+    }
+    return false
+  }
 }
 
 const config = new Config()
