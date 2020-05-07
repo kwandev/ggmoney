6f845f6acd6d01e1bc600c96b0f0e3c9f7030f76
khwan <rudghks7816@gmail.com>
2020-05-07 21:57:25 +0900

    feat: 메인페이지 이미지, 문구 추가

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -3,18 +3,20 @@
     <div class="container">
       <search-form />
 
-      <place-list />
+      <place-list v-if="total" />
+
+      <div v-if="total === 0" class="welcome">
+        <p>지역, 상호명을 이용해 경기지역화폐 가맹점을 검색해보세요</p>
+        <img src="img/welcome.png" alt="" />
+      </div>
 
       <place-map v-if="place.REFINE_WGS84_LAT && place.REFINE_WGS84_LOGT" />
     </div>
-
-    <!-- <portal to="portal">
-      <p>@@@@@@@@@@@@@@@This slot content will be rendered wherever the is located.</p>
-    </portal> -->
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import SearchForm from '@/components/form/SearchForm'
 import PlaceList from '@/components/place/PlaceList'
 import PlaceMap from '@/components/place/PlaceMap'
@@ -27,9 +29,10 @@ export default {
     PlaceMap
   },
   computed: {
-    place() {
-      return this.$store.state.place.place
-    }
+    ...mapState('place', {
+      place: 'place',
+      total: 'total'
+    })
   }
 }
 </script>
