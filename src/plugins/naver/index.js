112b201b16baf20375ac506c58aa9e5f86bf5686
khwan <rudghks7816@gmail.com>
2020-05-10 13:51:37 +0900

    feat: 네이버 API키 수정
    - 환경변수에서 key 가져오도록 수정

--- src/plugins/naver/index.js
+++ src/plugins/naver/index.js
@@ -1,11 +1,9 @@
-const URL = `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=lc2bcwtrpl`
+const URL = `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.VUE_APP_NAVER_API_KEY}`
 
 const script = document.createElement('script')
 
 if (script) {
   script.setAttribute('src', URL)
   script.id = 'naver-map-load'
-  // script.setAttribute('async', '')
-  // script.setAttribute('defer', '')
   document.head.appendChild(script)
 }
